99cafb95e24f7e410a68b53d3fb250eabe01f019
laurarnichols <32521892+laurarnichols@users.noreply.github.com>
2017-11-15 16:36:24 -0600

    Update rankFitBackgroundGen.m

--- rankFitBackgroundGen.m
+++ rankFitBackgroundGen.m
@@ -1,32 +1,26 @@
-function [ sorted ] = rankFitBackgroundGen( population, fitChunks, allCutIndex, wasCut ) %#ok<INUSL>
+function [ sorted ] = rankFitBackground( population, fitChunks, allCutIndex, wasCut, unshiftedChunks )
 %==========================================================================
 % This function will rank the different variable combinations
 % in the population based on the chi squared. It will
 % then sort the population and return the sorted array.
 %
 % Functions called:
-%           N/A
+%           getChiSquared - takes a function output from
+%                           MATLAB's fit function and finds
+%                           the chi squared given a data set
 %
 % Called by functions:
-%           GAFitBackground - fits all of the chunks together
-%                             along with a background
-%                             stretched exponential function
+%           cutGA - use GA to find best spot to cut linear
 %
 % Input data:
+%           x and y - data points
 %           population - contains all of the points within
 %                        the bounds to be ranked and sorted
-%           fitChunks - cell array of separated chunks that
-%                       have been shifted to be fit
-%           allCutIndex - vector containing indices of all 
-%                         cut points including linear cuts
-%           wasCut - vector containing boolean for if a 
-%                    given nonlinear chunk has been cut
-%           unshiftedChunks - cell array of separated chunks 
-%                             that have not been shifted 
 %
 % Output data:
 %           sorted - sorted population array based on chi
 %                    squared
+%           chiSquared - array of chiSquared values
 %
 % Created by: Laura Nichols
 % Creation date: 25 February 2017
@@ -47,22 +41,21 @@ numChunks = length(allCutIndex) - 1;
 countFit = 0;
 countLinear = 0;
 i = 1;
-
+    
 while i <= numChunks
-    % Nonlinear chunks
     countFit = countFit + 1;
     eval(sprintf('fitChunk%d = fitChunks{:,i};', countFit));
-    eval(sprintf('startHeight(countFit) = fitChunk%d(1,2);', countFit));
+    eval(sprintf('unshiftedChunk%d = unshiftedChunks{:,i};', countFit));
     i = i + 1;
-    
-    % Linear chunks
     if wasCut(countFit)
         countLinear = countLinear + 1;
         eval(sprintf('linearChunk%d = fitChunks{:,i};', countLinear));
+        eval(sprintf('unshiftedLinearChunk%d = unshiftedChunks{:,i};', countLinear));
         i = i + 1;
     end
 end
 
+%--------------------------------------------------------------------------
 % Check to make sure number of variables matches what
 % you would expect
 if numVars ~= (4*(countFit+1) + 2*countLinear)
@@ -70,12 +63,12 @@ if numVars ~= (4*(countFit+1) + 2*countLinear)
 end
 
 %--------------------------------------------------------------------------
+% Get background coefficients
+a1B = population(:,1); 
+a2B = population(:,2); 
+betaB = population(:,3); 
+tauB = population(:,4); 
 
-% Get background ceofficients
-a1B = population(:,1); %#ok<NASGU>
-a2B = population(:,2); %#ok<NASGU>
-betaB = population(:,3); %#ok<NASGU>
-tauB = population(:,4); %#ok<NASGU>
 %--------------------------------------------------------------------------
 % Get other nonlinear coefficients 
 
@@ -85,10 +78,8 @@ a2 = zeros(nInds,countFit);
 beta = zeros(nInds,countFit);
 tau = zeros(nInds,countFit);
 
-% Set up loop variable
-count = 0;
-
 % Populate coefficients
+count = 0;
 for i = 5:4:4*(countFit+1)
     count = count + 1;
     a1(:,count) = population(:,i);
@@ -117,21 +108,14 @@ for i = start:2:finish
 end
 
 %--------------------------------------------------------------------------
-% Fit each combo in your population and get
-% the chi squared
-
-% Preallocate for speed
+% Try to fit each combo in your population
 chiSquared = zeros(nInds,1);
-
-% Fit
 for i = 1:nInds
-    % Nonlinear
     for j = 1:countFit
         eval(sprintf('yFit = (a1(i,j) - a2(i,j)*exp(-(fitChunk%d(:,1)/tau(i,j)).^beta(i,j))) + (a1B(i) - a2B(i)*exp(-(fitChunk%d(:,1)/tauB(i)).^betaB(i)));', j, j))
         eval(sprintf('chiSquared(i) = chiSquared(i) + sum((yFit - fitChunk%d(:,2)).^2)/length(fitChunk%d(:,1));', j, j));
     end
     
-    % Linear
     for j = 1:countLinear
         eval(sprintf('yFit = (m(i,j)*linearChunk%d(:,1) + b(i,j)) + (a1B(i) - a2B(i)*exp(-(linearChunk%d(:,1)/tauB(i)).^betaB(i)));', j, j));
         eval(sprintf('chiSquared(i) = chiSquared(i) + sum((yFit - linearChunk%d(:,2)).^2)/length(linearChunk%d(:,1));', j, j));
@@ -142,10 +126,7 @@ end
 % Sort by chiSquared
 [~, index] = sort(chiSquared);
 
-% Preallocate for speed
 sorted = zeros(size(population));
-
-% Put in sorted array to return
 for i = 1:length(population)
     sorted(i,:) = population(index(i),:);
 end
